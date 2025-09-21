1b8641a52371987f2d9e7a8a2e48dbc40bfd0f0c
Ivan Trunaev <trunaev.ivan@icloud.com>
2025-09-21 08:37:21 +0500

    Fist release

--- game.pl
+++ game.pl
@@ -5,5 +5,31 @@ use warnings;
 
 say "Hello, neo!";
 
+sub confirm {
+	my $msg = shift;
+	print $msg . ' [Y/N] > ';
+	my $c = lc <>;
+	chomp $c;
+	return 1 if ($c eq 'y');
+	return 0 if ($c eq 'n');
+	return confirm ($msg);
+}
+
+my $is_neo = confirm ("You a really, neo?");
+my $name;
+
+if ($is_neo) {
+	die "Bad Karma, you are lier: Neo is cinema percon in Matrix the Folk! Don't try to lie to God! Bie";
+}
+
+print "What's you real name, dude? > ";
+my $name = <>;
+chomp $name;
+
+if (lc ($name) =~ /kirill philipp filipp satan murder killer hell dead zombie demon putin trump jesus jeuda/) {
+	die "It's name not for you, don't try to start this again!";
+} 
+
+say "Welcome to the game dear friend $name, me is God! My name is Yahwe! You know it!";
 
 1;
