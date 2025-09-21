c9daf7d81d15467fcf75f42b78bc9caa0e19b8d1
Ivan Trunaev <trunaev.ivan@icloud.com>
2025-09-21 08:46:59 +0500

    I teach good of fuck off predict the future

--- game.pl
+++ game.pl
@@ -23,6 +23,8 @@ sub confirm {
 my $is_neo = confirm ("You a really, neo?");
 my $name;
 
+sleep 1;
+
 if ($is_neo) {
 	die "Bad Karma, you are lier: Neo is cinema percon in Matrix the Folk! Don't try to lie to God! Bie";
 }
@@ -30,10 +32,32 @@ if ($is_neo) {
 print "What's you real name, dude? > ";
 my $name = input();
 
+sleep 1;
+
 if (lc ($name) =~ /kirill philipp filipp satan murder killer hell dead zombie demon putin trump jesus jeuda/) {
 	die "It's name not for you, don't try to start this again!";
 } 
 
 say "Welcome to the game dear friend $name, me is God! My name is Yahwe! You know it!";
+
+sleep 1;
+
+my $c = confirm ("Want to try to get one pediction about your wishing from the Lord?");
+
+sleep 1;
+
+if ($c) {
+	print "Type you wishing > ";
+	my $wishing = input();
+	sleep 3;
+	if (rand() >= 0.5) {
+		say "It will be realised in future!";
+	} else {
+		say "It's unreal!";
+	}
+} else {
+	say "Very good!";
+}
+
 
 1;
